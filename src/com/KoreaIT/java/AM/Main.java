0111f951007097fa0aecf92c9900b4d3e3406067
kimsinha11 <tlsgk2013@gmail.com>
2023-03-09 17:51:49 +0900

    게시글 테스트 데이터 생성

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -5,14 +5,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	public static List<Article> articles = new ArrayList<>();
+
+	static int lastArticleId = 0;
+	static int count = 0;
 
 	public static void main(String[] args) {
-
+		makeTestData();
 		Scanner sc = new Scanner(System.in);
-		List<Article> articles = new ArrayList();
-
-		int lastArticleId = 0;
-		int count = 0;
 
 		while (true) {
 			String nowdate = Util.getNowDateStr();
@@ -42,11 +42,11 @@ public class Main {
 							article.regDate, article.updateDate, article.hit);
 				}
 
-				// delete/ modify
-				// 번호, 제목, 내용
 			} else if (command.startsWith("article detail ")) {
 				String[] detail = command.split(" ");
+
 				int 번호 = Integer.parseInt(detail[2]);
+
 				Article articlefound = null;
 
 				for (int i = 0; i < articles.size(); i++) {
@@ -65,8 +65,9 @@ public class Main {
 					articlefound.hit++;
 					System.out.printf("번호:%d\n제목:%s\n내용:%s\n날짜:%s\n업데이트날짜:%s\n조회수:%d\n", 번호, articlefound.title,
 							articlefound.body, articlefound.regDate, articlefound.updateDate, articlefound.hit);
+
 				}
-		
+			
 			} else if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
@@ -76,7 +77,7 @@ public class Main {
 
 				Article article = new Article(id, title, body, nowdate, nowdate, count);
 				articles.add(article);
-				System.out.printf("%d번글이 생성되었습니다.\n", id);
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 				lastArticleId++;
 			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
@@ -89,6 +90,23 @@ public class Main {
 		sc.close();
 	}
 
+	public static void makeTestData() {
+		System.out.println("테스트를 위한 데이터를 생성합니다.");
+
+		for (int i = 0; i < 3; i++) {
+
+			int 아이디 = lastArticleId + 1;
+			String 제목 = "게시물" + 아이디;
+			String 내용 = "게시물" + 아이디 + " 내용";
+			String 날짜 = Util.getNowDateStr();
+			String 업데이트날짜 = Util.getNowDateStr();
+			int 조회수 = count;
+
+			Article article = new Article(아이디, 제목, 내용, 날짜, 날짜, 조회수);
+			articles.add(article);
+			lastArticleId++;
+		}
+	}
 }
 
 class Article {
@@ -99,6 +117,14 @@ class Article {
 	String body;
 	int hit;
 
+	void modifyArticle(int id, String title, String body, String regDate, String updateDate) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+	}
+
 	public Article(int id, String title, String body, String regDate, String updateDate, int hit) {
 		this.id = id;
 		this.regDate = regDate;
@@ -107,4 +133,5 @@ class Article {
 		this.title = title;
 		this.hit = hit;
 	}
+
 }
--- src/com/KoreaIT/java/AM/Util.java
+++ src/com/KoreaIT/java/AM/Util.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class Util {
-	
+	//포맷팅 현재 날짜/시간 반환 String타입
 	public static String getNowDateStr() {
 		Date now = new Date();
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
